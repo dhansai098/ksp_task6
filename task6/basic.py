0b166dd28ff939f941aa813062f42727a93bb682
B.Dhansai <150512590+dhansai098@users.noreply.github.com>
2024-02-28 18:59:58 +0530

    Add files via upload

---
+++ task6/basic.py
@@ -0,0 +1,55 @@
+print("dhansai")
+x=10 
+y="vaishu"
+print(x)
+print(y)
+a=10
+print(type(a))
+b="vaishu"
+print(type(b))
+c=12.5
+print(type(c))
+d=True
+print(type(d))
+z=5
+x=12
+if z>x:
+    print("z is grater than x")
+else:
+    print("x is greater than z")
+num1=5
+num2=10
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 ** num2)
+k=5
+print(k)
+k+=3
+print(k)
+k-=3
+print(k)
+k*=5
+print(k)
+k/=2
+print(k)
+num3=8
+num4=9
+print(num3 == num4)
+print(num3 != num4)
+print(num3 < num4)
+print(num3 > num4)
+print(num3 < num4)
+print(num3 <= num4)
+print(num3 >= num4)
+x < 5 and  x < 10	
+x < 5 or x < 4
+not(x < 5 and x < 10)
+for x in range(9):
+  print(x)
+i = 5
+while i < 6:
+  print(i)
+  i += 2
